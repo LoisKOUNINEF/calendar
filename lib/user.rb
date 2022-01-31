6acdb31a9ffd986fa1d216da0b145255197fb260
LoisKOUNINEF <lkouninef@gmail.com>
2022-01-31 20:36:34 +0100

    finally user.rb complete. Thanks Mr Vidal.

--- lib/user.rb
+++ lib/user.rb
@@ -2,22 +2,22 @@ class User
   attr_accessor :email, :age
   @@all_users = []
 
-  def initialize(email_to_save,age_to_save)
-    @email = email_to_save
-    @age = age_to_save.to_i
+  def initialize(user_email,user_age)
+    @email = user_email
+    @age = user_age
     @@all_users << self
   end
 
-  def self.all
+  def self.all_users
     return @@all_users.to_s
   end
 
-  #def self.find_via_email
-   # @@all_users.each do |user|
-    #  if user.email == @email
-     #   return user.age
-     # end
-    #end
-  #end
+  def self.find_by_email(user_email)
+    @@all_users.each do |x|
+      if x.email == user_email
+        puts "Voici l'âge de l'utilisateur : #{x.age} ans."
+      end
+    end
+  end
 
 end
