fa147bc8b039af285e5f3a632301238bedc4d6da
LoisKOUNINEF <lkouninef@gmail.com>
2022-02-01 07:35:24 +0100

    commit clean & added compo.

--- app.rb
+++ app.rb
@@ -4,4 +4,9 @@ Bundler.require
 require_relative 'lib/user'
 require_relative 'lib/event'
 
-binding.pry
+pepito = User.new("pepito@pepito.com", 29)
+pepita = User.new("pepita@pepita.com", 31)
+
+le_gouter = Event.new("2022-02-02 16:30", 30, "Le goûter c'est sacré.", [pepito, pepita])
+
+le_gouter.age_analysis
--- lib/event.rb
+++ lib/event.rb
@@ -7,31 +7,50 @@ class Event
     @duration = duration
     @title = title
     @attendees = attendees
-    @end_date = @start_date + (@duration * 60)
   end
 
   def postpone_24h
     @start_date = @start_date + 86400 #(24*60*60=86400)
   end
 
+  def end_date
+    return @start_date + (@duration * 60)
+  end
+
   def is_past
-     return @start_date < Time.now
+   return @start_date < Time.now
+ end
+
+ def is_future
+  return @start_date > Time.now
+end
+
+def is_soon
+  return Time.now > (@start_date - 30*60)
+end
+
+def age_analysis
+  age_array = []
+  average = 0
+
+  @attendees.each do |attendee|
+    age_array << attendee.age
+    average = average + attendee.age
   end
 
-  def is_future
-    return @start_date > Time.now
-  end
+  average = average / @attendees.length
 
-  def is_soon
-    return Time.now > (@start_date - 30*60)
-  end
+  puts "Voici les âges des participants :"
+  puts age_array.join(",")
+  puts "La moyenne d'âge est de #{average} ans."
+end
 
-  def event_to_s
-    puts "Event Title : #{@title}."
-    puts "Start Date and Time : #{@start_date}."
-    puts "Event Duration : #{@duration} minutes."
-    puts "End Date and Time : #{@end_date}."
-    print [attendees].to_s.delete'"[]'
-  end
+def event_to_s
+  puts "Event Title : #{@title}."
+  puts "Start Date and Time : #{@start_date}."
+  puts "Event Duration : #{@duration} minutes."
+  print "Event attendees :"
+  puts @attendees.join(",")
+end
 
 end
